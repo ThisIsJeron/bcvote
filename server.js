b65a69ddfe9035ac6975456b6eba7d085209d92b
Jeron Wong <jeron@protonmail.ch>
2017-05-07 11:32:07 -0700

    Add website

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var client = require('twilio')(accountSid, authToken);
 client.messages.create({
     to: "+14088217529",
     from: "+14085603576",
-    body: "Hello, please indicate who you are voting for",
+    body: "Hello Jeron, please indicate your vote for the 2020 presidential election: ",
 }, function(err, message) {
     console.log(message.sid);
 });
@@ -22,8 +22,10 @@ client.messages.create({
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
-
-  twiml.message('Thanks for voting!');
+  var str1 = 'Your vote has been recorded. Your country thanks you for voting! Your block is: ';
+  var str2 = '0d75273fbae32797590a577acfdaf8fe022b12a8abf4a3549cc9a208cbc99d42';
+  var banana = str1.concat(str2);
+  twiml.message(banana);
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
