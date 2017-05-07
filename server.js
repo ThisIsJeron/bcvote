7b703540ad7226952bfe7b8b5d14f22d0dfa2727
Jeron Wong <jeron@protonmail.ch>
2017-05-07 10:15:33 -0700

    Fixed server.js
    use sdk version 3.x

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var http = require('http');
-var express = require('express');
-var twilio = require('twilio');
+const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-var app = express();
+const app = express();
 
 // Twilio Credentials
 var accountSid = 'ACa2c293449cb04276aa45cdd8ac130342';
@@ -14,20 +14,21 @@ var client = require('twilio')(accountSid, authToken);
 client.messages.create({
     to: "+14088217529",
     from: "+14085603576",
-    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+    body: "Hello, please indicate who you are voting for",
 }, function(err, message) {
     console.log(message.sid);
 });
 
 
-app.post('/sms', function(req, res) {
-  var twilio = require('twilio');
-  var twiml = new twilio.TwimlResponse();
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
   twiml.message('Thanks for voting!');
+
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1337, function () {
-  console.log("Express server listening on port 1337");
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
 });
