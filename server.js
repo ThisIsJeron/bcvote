eb8299effcd3eb6bd47aaf3eb268c0be8bc9bf70
Jeron Wong <jeron@protonmail.ch>
2017-05-07 10:01:41 -0700

    make bcvote private again

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ var twilio = require('twilio');
 
 var app = express();
 
+// Twilio Credentials
+var accountSid = 'ACa2c293449cb04276aa45cdd8ac130342';
+var authToken = 'ed115b94915402b317ff74151b981198';
+
+//require the Twilio module and create a REST client
+var client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+    to: "+14088217529",
+    from: "+14085603576",
+    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+}, function(err, message) {
+    console.log(message.sid);
+});
+
+
 app.post('/sms', function(req, res) {
   var twilio = require('twilio');
   var twiml = new twilio.TwimlResponse();
